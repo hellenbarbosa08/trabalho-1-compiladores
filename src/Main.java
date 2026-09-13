009ed3f883b0b9c19d8b01c4f0d54085a1b7ee2c
Hellen <hellenbarbosan@gmail.com>
2026-09-13 12:43:13 -0300

    interpretador final

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,21 @@ public class Main {
     public static void main(String[] args) {
 
         String input = """
-            let a = 67 + 0;
-            let b = 21 + 2;
+            let a = 16 + 18;
+            let b = 225/ 5;
+            let c = a * 4;
             print a + b;
+            print c - 10;
             """;
 
         Parser parser = new Parser(input.getBytes());
 
         parser.parse();
+
+        System.out.println("TRADUCAO:");
+        System.out.println(parser.output());
+
+        System.out.println("RESULTADO:");
 
         Interpretador interpretador =
             new Interpretador(parser.output());
